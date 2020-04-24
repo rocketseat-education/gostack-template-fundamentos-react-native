7f8c56be0b4a33146869ab433bc1573b0e3108ac
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-04-24 11:01:09 -0300

    fix: add import for intl

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('react-native').unstable_enableLogBox();
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import { AppRegistry } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
