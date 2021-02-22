f11b4e7f29495ff14cf28ff9773324fd871d81d0
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-02-22 16:16:58 -0300

    Final version
    Build correctly the cart content giving it the total count of items in the cart and it's total value that can be acessed in any place at the application

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.quantity * product.price;
 
-    return formatValue(0);
+      return accumulator + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const productsCount = products.reduce((accumulator, product) => {
+      return accumulator + product.quantity;
+    }, 0);
 
-    return 0;
+    return productsCount;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,31 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.quantity * product.price;
 
-    return formatValue(0);
+      return accumulator + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const productsCount = products.reduce((accumulator, product) => {
+      return accumulator + product.quantity;
+    }, 0);
 
-    return 0;
+    return productsCount;
   }, [products]);
 
   return (
